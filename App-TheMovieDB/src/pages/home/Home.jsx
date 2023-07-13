77572c2c99bc90563c8789f2f6d720d74284e41a
Alvaro <Roekan03@gmail.com>
2023-07-13 16:02:43 +0200

    implement password security  dot env

--- App-TheMovieDB/src/pages/home/Home.jsx
+++ App-TheMovieDB/src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Home.css'
+import { bringCharacters } from '../../services/apiCalls'
 
 export const Home = () => {
+
+
+  useEffect(()=>{
+
+    bringCharacters();
+
+
+  },[])
+
+
+
+
+
+
   return (
 
     <div>Home</div>
-    
+
   )
 }
---
+++ App-TheMovieDB/src/services/apiCalls.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+
+
+
+
+const ApiKey = import.meta.env.VITE_SECRETKEY
+// require('dotenv').config()
+
+const peliculas = `https://api.themoviedb.org/3/movie/upcoming?api_key=${ApiKey}&language=en-US&page=1`
+
+
+
+export const bringCharacters = async () => {
+    const {data} = await axios.get(`${peliculas}`)
+    console.log(data.results)
+
+    return data.results
+}
