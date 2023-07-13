489a7a8bbaf90c8914ecaf91d31d1f17328474ee
Alvaro <Roekan03@gmail.com>
2023-07-13 13:49:17 +0200

    implement Header

--- App-TheMovieDB/src/common/header/Header.jsx
+++ App-TheMovieDB/src/common/header/Header.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
+import { Col, Container, Row } from 'react-bootstrap';
+
+
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <Container fluid className=' box-header'>
+ 
+          <Row className='d-flex align-items-center justify-content-around '>
+            <Col className='d-flex align-items-center justify-content-center  py-2' xs={12} md={1} lg={1}>
+              <img className='logo-header' src="./../../public/logo/logo-invertido.png"  />
+            </Col>
+            <Col className='d-flex align-items-center justify-content-left  py-2' xs={4} md={4} lg={4}>
+              
+            </Col>
+            <Col  xs={12} md={6} lg={6}>
+              <Row className=' d-flex align-items-center justify-content-around  py-1 '>
+                <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/" className='link-header' >Home</a> </Col>
+                <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/top20" className='link-header' >Top 20</a> </Col>
+                <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/search" className='link-header' >Buscador</a> </Col>
+              </Row>
+            </Col>
+
+        
+      </Row>
+    </Container>
+    </>
+    
   )
 }
--- App-TheMovieDB/src/pages/home/Home.jsx
+++ App-TheMovieDB/src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 
 export const Home = () => {
   return (
+
     <div>Home</div>
+    
   )
 }
