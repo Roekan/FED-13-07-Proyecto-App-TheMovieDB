f0f47fe56376c60a5965ad8e34b7c0292402d3e9
Alvaro <Roekan03@gmail.com>
2023-07-14 15:16:16 +0200

    change url images

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -11,14 +11,14 @@ export const Header = () => {
  
           <Row className='d-flex align-items-center justify-content-around '>
             <Col className='d-flex align-items-center justify-content-center  py-2' xs={12} md={1} lg={1}>
-              <img className='logo-header' src="./../../public/logo/logoInvertido.png"  />
+              <img className='logo-header' src="https://roekan.github.io/FED-13-07-Proyecto-App-TheMovieDB/public/logo/logoInvertido.png"  />
             </Col>
             <Col className='d-flex align-items-center justify-content-left  py-2' xs={4} md={4} lg={4}>
               
             </Col>
             <Col  xs={12} md={6} lg={6}>
               <Row className=' d-flex align-items-center justify-content-around  py-1 '>
-                <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/FED-13-07-Proyecto-App-TheMovieDB/" className='link-header' >Home aaa</a> </Col>
+                <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/FED-13-07-Proyecto-App-TheMovieDB/" className='link-header' >Home</a> </Col>
                 <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/FED-13-07-Proyecto-App-TheMovieDB/top20" className='link-header' >Top 20</a> </Col>
                 <Col className='d-flex align-items-center justify-content-center box-link-header' xs={3}> <a href="/FED-13-07-Proyecto-App-TheMovieDB/search" className='link-header' >Buscador</a> </Col>
               </Row>
