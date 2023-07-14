9dd66e915ea115360686b0348ed668d434bcc617
Alvaro <Roekan03@gmail.com>
2023-07-14 11:23:20 +0200

    implement deploy github

--- App-TheMovieDB/src/services/apiCalls.js
+++ App-TheMovieDB/src/services/apiCalls.js
@@ -4,12 +4,39 @@ import axios from 'axios'
 const ApiKey = import.meta.env.VITE_SECRETKEY
 // require('dotenv').config()
 
-const films = `https://api.themoviedb.org/3/movie/upcoming?api_key=${ApiKey}&language=en-US&page=1`
+const peliculas = `https://api.themoviedb.org/3/movie/upcoming?api_key=${ApiKey}&language=en-US&page=1`
 
 
 export const bringFilms = async () => {
-    const {data} = await axios.get(`${films}`)
+    const {data} = await axios.get(`${peliculas}`)
     console.log(data.results)
 
     return data.results
 }
+
+
+
+
+
+// import axios from 'axios'
+
+
+// const ApiKey = import.meta.env.VITE_SECRETKEY
+// // require('dotenv').config()
+
+// const films = `https://api.themoviedb.org/3/movie/upcoming?page=1`
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer 210d6a5dd3f16419ce349c9f1b200d6d'
+//     }
+//   }
+
+// export const bringFilms = async () => {
+//     const {data} = await axios.get(films,options)
+//     console.log(data.results)
+
+//     return data.results
+// }
