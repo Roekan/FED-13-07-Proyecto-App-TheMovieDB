7b5ed9cfb12de18e1ced193bb09a8594a9a13872
Alvaro <Roekan03@gmail.com>
2023-07-20 15:26:24 +0200

    fix routes git

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
+import {  HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from "./app/store";
 import { PersistGate } from 'redux-persist/es/integration/react'
@@ -18,9 +17,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       {/* Aplicamos la puerta de persistencia en el local Storage */}
       <PersistGate loading={null} persistor={persistor}>
         {/* BrowserRouter nos implementa la navegación entre páginas */}
-        <BrowserRouter basename='/FED-13-07-Proyecto-App-TheMovieDB/'>
+        {/* <BrowserRouter basename='/FED-13-07-Proyecto-App-TheMovieDB/'> */}
+          <HashRouter>
           <App />
-        </BrowserRouter>
+          </HashRouter>
+        {/* </BrowserRouter> */}
       </PersistGate>
     </Provider>
   </React.StrictMode>
