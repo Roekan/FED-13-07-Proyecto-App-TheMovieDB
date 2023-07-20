b7fbf9026d0195df4ff9bfa396a41186a0d5b7d9
Alvaro <Roekan03@gmail.com>
2023-07-20 12:46:22 +0200

    add readme images

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
             <a href="/FED-13-07-Proyecto-App-TheMovieDB/" ><img className='logo-header' src={Logo} /></a>
             </Col>
             <Col  xs={12} md={9} lg={9}>
-              <Row className=' d-flex align-items-center justify-content-center pe-5 py-1 '>
+              <Row className=' d-flex align-items-center justify-content-center py-1 '>
                 <Col className='d-flex align-items-center justify-content-center py-1 box-link-header ' xs={6} md={3}> <NavLink to="/" className= "link-header">Películas</NavLink> </Col>
                 <Col className='d-flex align-items-center justify-content-center py-1 box-link-header ' xs={6} md={3}> <NavLink to="series" className=' link-header' >Series</NavLink> </Col>
                 <Col className='d-flex align-items-center justify-content-center py-1 box-link-header ' xs={6} md={3}> <NavLink to="top20" className=' link-header' >Top 20</NavLink> </Col>
